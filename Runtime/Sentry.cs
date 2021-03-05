de9efe57d5291f511e8aa075fdfd9cc259286e55
Mike Belotti <mikebelotti@transfrvr.com>
2021-03-05 09:16:33 -0500

    1.0.3.1 was not proper semver for Unity; bumping to 1.0.4

--- Runtime/Sentry.cs
+++ Runtime/Sentry.cs
@@ -265,7 +265,7 @@ namespace Sentry
     public class SdkVersion
     {
         public string name = "sentry.unity.lite";
-        public string version = "1.0.3.1";
+        public string version = "1.0.4";
     }
 
     [Serializable]
